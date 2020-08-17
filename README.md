33d87e0c7e1513e2af4fc5e21bf51e44c9bca904
Vngdv <vngdv@endlessblack.space>
2020-08-17 18:57:06 +0200

    Added some more user friendly messages

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # MemeLeave
 
-This is a Discord bot used to play a sound after someone leaves an channel.
+This is a Discord bot used to play a random sound after someone leaves a channel.
 
-## How to run
+## How to run the MemeLeave with **Docker** and **Docker Compose**
 
-This bot is intended to be used as a Docker container.
+It is really simple you just have to follow these steps:
 
-Steps: 
-
-* Put your sounds in the `sounds` folder one of them will be picked at random when someone leaves.
-* Change the Bot Token in the `.env` file.
-* Build and run the container with `docker-compose up -d` for a detached startup.
+* Make sure you have Docker and Docker Compose installed.
+* Put your sounds in the 'sounds' folder one of them will be picked at random when someone leaves.
+* Change the Bot Token in the '.env' file.
+* Build and run the container with 'docker-compose up -d' for a detached startup.
--- index.js
+++ index.js
@@ -1,42 +1,65 @@
-const path = require('path');
-var fs = require('fs');
+// Configuration constats
+const defaultVolume = 0.6;
 
-const soundFiles = fs.readdirSync("sounds")
-    .map(file => {
-        return path.join("sounds", file);
-    });
+function loadSoundFiles() {
+    const path = require('path');
+    const fs = require('fs');
 
+    return fs.readdirSync("sounds")
+        .filter(fname => fname.endsWith(".mp3"))
+        .map(fname => {
+            return path.join("sounds", fname);
+        });
+}
 
+const soundFiles = loadSoundFiles();
+
+function randomFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+// Check if there were sound files and stop if there are none.
+if (soundFiles.length == 0) {
+    console.error('No sound files found! Stopping the bot');
+    process.exit(1);
+}
+
+// If the process is still running print out the amount of sounds we loaded.
+console.log('Loaded ' + soundFiles.length + ' soundes!')
+
+// Initialize discord and add some events
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-
-
 client.on('ready', () => {
-    client.user.setStatus('online')
+    client.user.setStatus('online');
     console.log(`Logged in as ${client.user.tag}!`);
-    console.log('Loaded ' + soundFiles.length + ' soundes!')
+
 });
 
+// Voice state update is used to determine if a user has left the voice chat.
 client.on('voiceStateUpdate', function (oldMember, newMember) {
     if (client.user.id != newMember.id && newMember.channel == null) {
         joinAndPlay(oldMember.channel);
     }
 });
 
-async function joinAndPlay(channel) {
-    var connection = await channel.join();
-    var randomItem = soundFiles[Math.floor(Math.random() * soundFiles.length)];
-    console.log('Playing ' + randomItem);
-    var dispatcher = connection.play(randomItem, { volume: 0.3 });
+function joinAndPlay(channel) {
+    channel.join().then(conn => {
+        var randomItem = randomFromArray(soundFiles);
 
-    dispatcher.on('finish', () => {
-        dispatcher.destroy();
-        connection.disconnect();
+        console.log('Playing ' + randomItem);
+        var dispatcher = conn.play(randomItem, { volume: defaultVolume });
+
+        dispatcher.on('finish', () => {
+            dispatcher.destroy();
+            conn.disconnect();
+        });
     });
-
-
 }
 
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN).catch(err => {
+    console.error('Client was not able to login to Discord.\n' + err);
+    process.exit(1);
+});
